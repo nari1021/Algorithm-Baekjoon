1836ca98d910293a29d193fbf20f06082afda865
nari1021 <y.shhh000@gmail.com>
2021-04-07 16:53:34 +0900

    Update 10093.java

--- 0x02_기초코드 작성요령2/Main10093.java
+++ 0x02_기초코드 작성요령2/Main10093.java
@@ -1,28 +1,26 @@
-import java.io.*;
 import java.util.*;
+import java.io.IOException;
 
 public class Main10093 {
     public static void main(String[] args) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         Scanner sc = new Scanner(System.in);
 
-        long A = sc.nextLong();
+        Long A = sc.nextLong();
         long B = sc.nextLong();
 
         if (A < B) {
-            bw.write((B-A-1) + "\n");
+            System.out.println((B-A-1) + "\n");
             for (long i = A + 1; i < B; i++) {
-                bw.write(i + " ");
+                System.out.println(i + " ");
             }
         }
         else if (A > B) {
-            bw.write((A-B-1) + "\n");
+            System.out.println((A-B-1) + "\n");
             for(long i = B + 1; i < A; i++) {
-                bw.write(i + " ");
+                System.out.println(i + " ");
             }
         }
         else
-            bw.write("0");
-        bw.flush(); bw.close();
+            System.out.println("0");
     }
 }
