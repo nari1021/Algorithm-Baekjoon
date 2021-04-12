2d416f0ad2a9b4efb2c7d0c5c363e78b7aaeec7c
nari1021 <y.shhh000@gmail.com>
2021-04-12 21:11:52 +0900

    Update 2443.java

--- 0x02_기초코드 작성요령2/Main2443.java
+++ 0x02_기초코드 작성요령2/Main2443.java
@@ -2,6 +2,19 @@ import java.io.*;
 
 public class Main2443 {
     public static void main(String[] args) throws IOException {
-        System.out.println("hi");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int index = 0;
+        int N = Integer.parseInt(br.readLine());
+        while (index < N) {
+            for(int i = 0; i < index; i++)
+                bw.write(" ");
+            for(int i = 1; i < 2 * (N - index); i++)
+                bw.write("*");
+            bw.newLine();
+            index++;
+        }
+        br.close(); bw.flush(); bw.close();
     }
 }
