1191dfaa08b18f1d78ebe2e6f3db6d21efa94a08
nari1021 <y.shhh000@gmail.com>
2022-06-02 23:59:21 +0900

    add: 2438

--- Random/Main1182.java
+++ Random/Main1182.java
@@ -1,5 +1,9 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main1182 {
     public static void main(String[] argv) throws IOException {
