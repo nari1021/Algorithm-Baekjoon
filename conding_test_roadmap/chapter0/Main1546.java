baed3f996a42ad028871522af67b4de67ea5a15c
nari1021 <y.shhh000@gmail.com>
2022-06-09 22:48:50 +0900

    add: solved 1546

---
+++ conding_test_roadmap/chapter0/Main1546.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main1546 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); // 입력 개수
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int max = 0;
+        double sum = 0.0;
+
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+
+            if (value > max) {
+                max = value;
+            }
+            sum += value;
+        }
+        System.out.println(((sum / max) * 100.0) / N);
+    }
+}
